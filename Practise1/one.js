068609b7d995a11879f1f9699082374197c111bd
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-08-17 05:30:41 +0000

    learnt one

--- Practise1/one.js
+++ Practise1/one.js
@@ -1,16 +1,39 @@
-const body = document.querySelector('body');
-const boxs = document.querySelectorAll('boxs');
+// const { createElement } = require("react");
 
-boxs.forEach((b) => {
-    console.log(b);
-    b.addEventListener('click', (e) => {
+const body = document.querySelector('body');
+const boxs = document.querySelectorAll('.box');
+
+
+
+
+const addNewDiv = (colourName) => {
+    const div = document.createElement('div');
+    div.id = (`${colourName}`);
+    div.className = 'box';
+    div.innerHTML = `Box 3`;
+    document.querySelector('.container').appendChild(div);
+    div.addEventListener('click', (e) => {
+        body.style.backgroundColor = e.target.id; 
+    });
+}
+
+addNewDiv('brown');
+addNewDiv('yellow');
+
+
+
+boxs.forEach((box) => {
+    console.log(box);
+    box.addEventListener('click', (e) => {
         console.log(e);
         console.log(e.target);
-        if (e.target.id === "blue") {
-            body.style.backgroundColor = e.target.id;
+        // if (e.target.id === "blue") {
+        //     body.style.backgroundColor = e.target.id;
 
-        }
-        else
-            body.style.backgroundColor = e.target.id;
+        // }
+        // else if (e.target.id === "green") {
+        //     body.style.backgroundColor = e.target.id;
+        // }
+         body.style.backgroundColor = e.target.id; 
     })
 })
